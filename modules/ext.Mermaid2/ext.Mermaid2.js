0c89623d2b8f13314b3ab877935d49f23ea38b17
tesinormed <tesinormed@5505.industries>
2025-08-18 13:31:34 -0700

    version 0.6.0

--- modules/ext.Mermaid2/ext.Mermaid2.js
+++ modules/ext.Mermaid2/ext.Mermaid2.js
@@ -1,10 +1,25 @@
-$( function () {
+$( () => {
+	const rootClasses = document.documentElement.classList;
+	let darkMode = false;
+
+	if ( rootClasses.contains( 'skin-theme-clientpref-night' ) ) {
+		darkMode = true;
+	} else if ( window.matchMedia( '(prefers-color-scheme: dark)' ).matches &&
+		rootClasses.contains( 'skin-theme-clientpref-os' )
+	) {
+		darkMode = true;
+	}
+
 	mermaid.initialize( {
 		securityLevel: 'strict',
 		startOnLoad: false,
+		logLevel: 'error',
+		theme: darkMode ? 'dark' : 'neutral'
 	} );
-} );
 
-mw.hook( 'wikipage.content' ).add( ( $content ) => {
-	mermaid.run();
+	mw.hook( 'wikipage.content' ).add( ( $content ) => {
+		mermaid.run( {
+			nodes: $content.find( '.mermaid' )
+		} );
+	} );
 } );
